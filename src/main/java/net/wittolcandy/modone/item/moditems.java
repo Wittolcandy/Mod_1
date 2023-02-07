5f2bc57a84a50f211740bfab5d6b85ca4b527115
Wittolcandy <gage.r.johnson888@gmail.com>
2023-02-07 15:00:18 -0500

    e

--- src/main/java/net/wittolcandy/modone/item/moditems.java
+++ src/main/java/net/wittolcandy/modone/item/moditems.java
@@ -1,6 +1,9 @@
 package net.wittolcandy.modone.item;
 
-
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,10 +15,24 @@ public class moditems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, modone.MOD_ID);
 
-public static final RegistryObject<Item> APPLE_JUICE = ITEMS.register("apple_juice",
-        () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> APPLE_JUICE = ITEMS.register("apple_juice",
+            () -> new Item(new Item.Properties().food(Foods.Apple_Juice)));
+
+    public static class Foods {
+        public static final FoodProperties Apple_Juice = new FoodProperties.Builder()
+                .nutrition(6)
+                .saturationMod(4)
+                .alwaysEat()
+                .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400, 3), 1.0f)
+                .build();
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
+
+
+
+
+
